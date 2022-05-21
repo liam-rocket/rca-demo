62436e19b141f06c064addba8c1f1352da4b5aa0
Liam Leung <liam.leung@binery.co>
2022-05-22 00:11:21 +0800

    feat: add back sequelize errors

--- Module 4/4.1.6/middlewares/error.middleware.js
+++ Module 4/4.1.6/middlewares/error.middleware.js
@@ -1,27 +1,27 @@
 /* eslint-disable import/extensions */
 /* eslint-disable no-unused-vars */
 
-// import sequelizePackage from 'sequelize';
+import sequelizePackage from 'sequelize';
 import HttpException from '../exceptions/HttpException.js';
 
-// const { ValidationError, DatabaseError } = sequelizePackage;
+const { ValidationError, DatabaseError } = sequelizePackage;
 
 function errorMiddleware(error, request, response, next) {
-  // if (error instanceof DatabaseError) {
-  //   console.error('This is a database error!');
-  //   console.error(error);
-  //   response.status(500).json({
-  //     message: error.message,
-  //   });
-  // }
+  if (error instanceof DatabaseError) {
+    console.error('This is a database error!');
+    console.error(error);
+    response.status(500).json({
+      message: error.message,
+    });
+  }
 
-  // if (error instanceof ValidationError) {
-  //   console.error('This is a validation error!');
-  //   console.error(error);
-  //   response.status(500).json({
-  //     message: error.errors[0].message,
-  //   });
-  // }
+  if (error instanceof ValidationError) {
+    console.error('This is a validation error!');
+    console.error(error);
+    response.status(500).json({
+      message: error.errors[0].message,
+    });
+  }
 
   if (error instanceof HttpException) {
     const status = error.status || 500;
