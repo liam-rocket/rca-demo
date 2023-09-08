0e0f3f4ae64415cb4d3c133fdeac9b784775179d
Liam Leung <liam.leung@binery.co>
2023-09-08 11:01:15 +0700

    feat: revert changes 8.4

--- Module 8/8.4/src/pages/Home/index.jsx
+++ Module 8/8.4/src/pages/Home/index.jsx
@@ -9,6 +9,27 @@ const Home = () => {
   const [sum, dispatch] = useReducer((state, action) => {
     return state + action;
   }, 0);
+  const [test, twoDispatch] = useReducer((state, action) => {
+    return state + action;
+  }, 0);
+
+  const [isJs, setIsJs] = useState(true);
+  const [questions, setQuestions] = useState({});
+
+  const importQuestions = async () => {
+    if (isJs) {
+      const questions = await import('./js.json');
+      setQuestions(questions.default);
+    } else {
+      setQuestions(await import('./react.json').default);
+    }
+  };
+
+  useEffect(() => {
+    importQuestions();
+  }, []);
+
+  console.log('questions: ', questions);
 
   return (
     <div>
@@ -21,7 +42,7 @@ const Home = () => {
           <a href="/about-us">About us</a>
         </li>
       </ul>
-      {params}
+      {sum}
       <button onClick={() => dispatch(1)}>Add 1</button>
     </div>
   );
