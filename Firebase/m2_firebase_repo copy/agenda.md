8b51a37a98a8211cdb516c808e6918e34a88125b
Liam Leung <liam.leung@binery.co>
2023-09-12 19:19:57 +0800

    chore: add comments

---
+++ Firebase/m2_firebase_repo copy/agenda.md
@@ -0,0 +1,8 @@
+# 12 Sep 2023
+
+## Agenda
+
+- go through latest changes
+- useContext
+- useReducer
+- custom hook
--- Firebase/m2_firebase_repo copy/src/hooks/useAuth.js
+++ Firebase/m2_firebase_repo copy/src/hooks/useAuth.js
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
 import { AuthContext } from '../contexts/authContext';
 
+// we only need to call useContext once throughout the codebase
 export const useAuth = () => useContext(AuthContext);
